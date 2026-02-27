0469a261e40011cb2720b1108a9c879aa9f0fdcd
ryan echternacht <ryan@echternacht.org>
2026-02-27 11:54:33 -0500

    much faster now

--- src/aoc_2025/day04.clj
+++ src/aoc_2025/day04.clj
@@ -64,11 +64,37 @@
 ;; for re-evaluation. after a few runs this should limit the amount
 ;; of re-evaluation significantly. 
 
+;; went ahead and did that, and it's much faster
+
+(defn make-adjancies [m [x y]]
+  (->> adjancies
+       (map (fn [[x2 y2]] [(+ x x2) (+ y y2)]))
+       (map m)
+       (filter identity)))
+
+(defn fastest-solve [file]
+  (let [start (build-map file)
+        final
+        (loop [m start
+               [p & ps] start]
+          #_(Thread/sleep 10)
+          (cond
+            (nil? p) m
+
+            (< (count-adjancies m p) 4)
+            (let [new-m (disj m p)
+                  new-ps (into ps (make-adjancies new-m p))]
+              (recur new-m new-ps))
+
+            :else (recur m ps)))]
+    (- (count start) (count final))))
+
 (comment
   (build-map "resources/day04-sample.txt")
 
   (def m (build-map "resources/day04-sample.txt"))
   (count-adjancies m [1 0])
+  (make-adjancies m [0 0])
 
   (solve "resources/day04-sample.txt")
   (solve "resources/day04.txt")
@@ -80,13 +106,19 @@
        build-map
        remove-pass
        count)
-  
+
   (faster-solve "resources/day04-sample.txt")
   (faster-solve "resources/day04.txt")
+
+  (fastest-solve "resources/day04-sample.txt")
+  (fastest-solve "resources/day04.txt")
 
   (time (solve-2 "resources/day04.txt"))
   ; 47502.452583 msecs
   (time (faster-solve "resources/day04.txt"))
   ; 541.654166 msecs
+  (time (fastest-solve "resources/day04.txt"))
+  ; 218.734792 msecs
 
+  ;
   )
